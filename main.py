70fc445cf441a678642df064a71dc4268cc95f4b
Felipe Villegas <felavid@gmail.com>
2021-06-30 15:08:30 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import teleport as tp
+
+# Pass x, y, z coordinates as integers
+# Found a ship wreck at (200, 120, -600)
+tp.teleport(200, 120, -600)
+
+# Teleport to original spawn point (0, 0, 0)
+# tp.teleport_world_spawn()
---
+++ teleport.py
@@ -0,0 +1,19 @@
+from mcpi.minecraft import Minecraft
+import time
+
+mc = Minecraft.create()
+
+
+def teleport_world_spawn():
+    time.sleep(10)
+    mc.player.setTilePos(0, 0, 0)
+
+
+def teleport(x, y, z):
+    time.sleep(10)
+    mc.player.setTilePos(x, y, z)
+
+
+def teleport_precise_pos(x, y, z):
+    time.sleep(10)
+    mc.player.setPos(x, y, z)
