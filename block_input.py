64c5efddc27685314882e549701f32ba20f487ba
Felipe Villegas <felavid@gmail.com>
2021-07-03 10:59:25 -0400

    saving progress...

--- block_input.py
+++ block_input.py
@@ -2,13 +2,13 @@ from mcpi.block import Block
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-block = int(input('Enter block id number:  '))
+block = int(input('Enter block id number: '))
 
 pos = mc.player.getTilePos()
 x = pos.x 
 y = pos.y
 z = pos.z 
 
-mc.setBlock(x, y-1, z, block)
+mc.setBlock(x, y, z, block)
 
 print("block: ", block)
--- set_blocks.py
+++
@@ -1,9 +0,0 @@
-from mcpi.minecraft import Minecraft
-mc = Minecraft.create()
-
-def place_block(x, y, z, block, *state):
-    if state:
-        mc.setBlock(x, y, z, block, state)
-    else:
-        mc.setBlock(x, y, z, block)
-
